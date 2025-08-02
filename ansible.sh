987e0cf04a4a5232cb3180c12154f7e1c29c358d
Rahul155505 <imrahulraghuvanshi@gmail.com>
2025-08-02 12:51:02 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,8 @@
 amazon-linux-extras install ansible2 -y
 yum install python3 python-pip python-dlevel -y
+
+FOR ME NOW
+sudo dnf update -y
+sudo dnf install -y python3-pip
+pip3 install --user ansible
+ansible --version
